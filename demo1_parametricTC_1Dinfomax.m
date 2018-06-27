946ededaf43d1500a902c48759152270341891c9
Jonathan Pillow <jpillow@gmail>
2018-06-27 00:50:02 -0400

    updates to demo1: improved comments and legibility

--- demo1_parametricTC_1Dinfomax.m
+++ demo1_parametricTC_1Dinfomax.m
@@ -6,158 +6,160 @@
 % 
 % Makes plots found in FIG 2 of [Pillow & Park 2016].
 
+setpaths; % initialize path to include correct directories
 
-%%  Set true tuning curve
+%%  Set true tuning curve for simulated neuron
 
 % grid over stimulus values "x"
-xlims = [-10, 10]; % limits of left and right edge
-dx = .05; % spacing x grid
+xlims = [-10, 10]; % range of the stimulus space
+dx = .05; % spacing of stimulus grid
 xx = (xlims(1)+dx/2:dx:xlims(2))'; % grid of x points
 xrnge = diff(xlims); % stimulus range
 nx = length(xx); % number of stimulus grid points
 
 % parameters of parametric TC (here an unnormalized Gaussian plus a
-% baseline, but you can write your own parametric function instead).
+% baseline, but you can write your own parametric function instead!).
 mutc = 3.5;  % tuning curve mean
 sigtc = 1;  % tuning curve width
 Amax = log(50);  % tuning curve amplitude
 bl = 2;  % baseline firing rate
+theta_true = [mutc,sigtc,Amax,bl];  % the true TC parameters
 
 % make tuning curve
 ftc0 = @(x,prs)(exp(prs(3)-(x-prs(1)).^2/(2*prs(2).^2))+prs(4)); % generic TC function
-ftc = @(x)(ftc0(x,[mutc,sigtc,Amax,bl]));  % true TC function
-tctrue = ftc(xx);  % true TC evaluated on grid
+ftctrue = @(x)(ftc0(x,theta_true));  % true TC function
+tctrue = ftctrue(xx);  % true TC evaluated on grid
 clf;subplot(221); plot(xx,tctrue);
-xlabel('stimulus value'); ylabel('spike rate (sp/s)'); 
+xlabel('stimulus'); ylabel('spike rate (sp/s)'); 
 
 % run short random experiment for initialization
-ntrain = 3;
-xtr = (rand(ntrain,1)*xrnge+xlims(1)); % some initial stimuli, randomly selected
-ytr = poissrnd(ftc(xtr)); % response of neuron to these stimuli
-clf; subplot(221); plot(xx,ftc(xx), xtr,ytr,'ro');
+ninit = 3; % number of randomly selected stimuli to use for initialization
+ntrials = 10; % number of adaptive trials to run for (after the initial ones)
+xtr = rand(ninit,1)*xrnge+xlims(1); % some initial stimuli, randomly selected
+ytr = poissrnd(ftctrue(xtr)); % response of neuron to these stimuli
+
+% If desired, plot initial TC and responses
+clf; plot(xx,ftctrue(xx), xtr,ytr,'ko', 'linewidth', 2);
 xlabel('stimulus value'); ylabel('spike count'); 
 title('true tuning curve and initial data');
-
-% for plotting
-colr = [.8 .8 .8];
+legend('true tuning curve', 'data');
 
 
-%% Set params for slice sampling (MCMC) of posterior over parameters
+%% Set up slice sampling (MCMC) of posterior over parameters
 
 % Set parameter ranges
-murnge = xlims*.98; % range for mu parameter
-sigrnge = [.1 20]; % range for sigma parameter
+murnge = xlims*.98; % range for mu parameter (mean of Gaussian bump)
+sigrnge = [.1 20]; % range for sigma parameter (width of Gaussian bump)
 Arnge = log([1 200]); % range for log amplitude of TC
 brnge = [.01 50]; % range for baseline of TC
 
 % Set bounds and options
-LB = [murnge(1); sigrnge(1); Arnge(1); brnge(1)];
-UB = [murnge(2); sigrnge(2); Arnge(2); brnge(2)];
-prs0 = (LB+UB)/2;
+LB = [murnge(1); sigrnge(1); Arnge(1); brnge(1)]; % lower bounds
+UB = [murnge(2); sigrnge(2); Arnge(2); brnge(2)]; % upper bounds
+prs0 = (LB+UB)/2; % initial parameters
 
-ns = 100;  % # samples to use
-nburn = 100;  % # samples to burn in
+nslice = 100;  % # samples of slice sampling to use for each trial
+nburn = 100;  % # samples to use for "burn in" on each trial
 
-% Function pointer for log-likelihood function
-flogpdf = @(prs)logliPoissonTCbd(prs,xtr,ytr,LB,UB);
-
-% run initial sample to get good initialization point
-psamps = mean(slicesample(prs0',50,'logpdf',flogpdf,'burnin', 1000));
+% sample posterior to get a good initialization point for MCMC 
+flogpdf = @(prs)logliPoissonTCbd(prs,xtr,ytr,LB,UB); % log-likelihood function
+thetasamps = mean(slicesample(prs0',50,'logpdf',flogpdf,'burnin', 1000)); % mean of posterior samples
 
 
-%% Run algorithm
-nTrials = 10; % number of total trials to run for (after the 3 initial ones above)
+%% Run adaptive stimulus selection algorithm
+
+% initialize plotting and variables to store
 trialNumsToPlot = [1,2,3,8]; % generate plots of TC estimate for each of these #s
-npl = 4; % number of plots
+npl = 4; % total number of plots
 jplot=1; % index variable for plots
-Errs = zeros(nTrials,2); % stores errors in posterior mean over parameters TC and posterior mean over TC
+colr = [.8 .8 .8]; % color for plotting posterior samples of TC
+Errs = zeros(ntrials,2); % stores errors in posterior mean over parameters TC and posterior mean over TC
+
+% main FOR loop
 for jj = 1:max(trialNumsToPlot)
 
     % set log pdf using all available data collected so far
-    flogpdf = @(prs)logliPoissonTCbd(prs,xtr,ytr,LB,UB); 
-    psamps = slicesample(psamps(end,:),ns,'logpdf',flogpdf,'burnin',nburn); % sample posterior over params
-    prsmu = mean(psamps)'; % mean of posterior over parameters (from samples)
+    flogpdf = @(prs)logliPoissonTCbd(prs,xtr,ytr,LB,UB);  % log-likelihood function
+    thetasamps = slicesample(thetasamps(end,:),nslice,'logpdf',flogpdf,'burnin',nburn); % sample posterior & compute mean
+    thetamu = mean(thetasamps)'; % mean of posterior over parameters (from samples)
 
     % compute posterior mean over TC
     % (note we could do this more efficiently WITHOUT a for loop!)
-    muTC = zeros(nx,1); 
-    for j = 1:ns 
-        tc = ftc0(xx,psamps(j,:));  % TC for the j'th sample
-        muTC = muTC+tc/ns; % posterior mean of TC
+    TCmu = zeros(nx,1); 
+    for j = 1:nslice 
+        tc = ftc0(xx,thetasamps(j,:));  % TC for the j'th sample
+        TCmu = TCmu+tc/nslice; % posterior mean of TC
     end
 
-    %% Compute predictive distributions
-    rmax = ceil(max(muTC)+sqrt(max(muTC))*4); % compute max expected spike count
-    rr = (0:rmax)'; % grid over spike counts
-    prr = zeros(rmax+1,nx,ns); % probability over spike counts, for each stim, for each sample
-    for j = 1:ns % loop over samples 
-        tc = (ftc0(xx,psamps(j,:))); % TC for the j'th sample
+    %% Compute predictive distribution of response  P(r)
+    rmax = ceil(max(TCmu)+sqrt(max(TCmu))*4); % max expected spike count given TC estimate
+    rr = (0:rmax)'; % grid over integer spike counts
+    prr = zeros(rmax+1,nx,nslice); % probability over spike counts, for each stim, for each sample
+    for j = 1:nslice % loop over samples 
+        tc = (ftc0(xx,thetasamps(j,:))); % TC for the j'th sample
         prr(:,:,j) = exp(bsxfun(@plus,bsxfun(@plus,rr*log(tc)',...
             -tc'), -gammaln(rr+1))); % Poisson probability over spike count for this sample, for each stimulus
     end
     
-    
     Pr = mean(prr,3);  % marginal response distribution (aka "posterior predictive")
-    Hr = -sum(Pr.*log(Pr))'; % marginal entropy of response H(r)
-    prr = max(prr,.00001); 
-    Hrtheta = -mean(sum(prr.*log(prr),1),3)'; % conditional entropy H(r|theta)
-    MI = Hr-Hrtheta; % Mutual information between stimulus and parameters
-    if any(isnan(MI)) | any(isinf(MI))
-        keyboard; % for debugging purposes (please report errors to pillow@princeton.edu)
-    end
+    Hr = -sum(Pr.*log2(Pr))'; % marginal entropy of response H(r)
+    prr = max(prr,1e-6); % make sure probabilities >= 0
+    Hrtheta = -mean(sum(prr.*log2(prr),1),3)'; % conditional entropy H(r|theta)
+    MI = Hr-Hrtheta; % Mutual information between parameters and response, for each candidate stimulus
 
-    % Select and present new stimulus
+    % Select optimal (infomax) stimulus
     [xnext,idx] = argmax(MI,xx);  % select stimulus with maximal MI
-    ynext = poissrnd(ftc(xnext)); % sample response to this stimulus under TC-Poisson model
-
-    %% ==== Make plots % ==============================
     
-    % Plot posterior over TC
-    if jj==trialNumsToPlot(jplot)
-        
-        % Plot TC samples 
-        subplot(2,npl,jplot);
-        for j = 1:ns
-            tc = ftc0(xx,psamps(j,:));
-            plot(xx,tc,'color', colr,'linewidth', 2); hold on;
+    % Present selected stimulus to (simulated) neuron
+    ynext = poissrnd(ftctrue(xnext)); % sample response to this stimulus under TC-Poisson model
+    
+    % add new (stimulus,response) pair to the dataset
+    xtr(end+1) = xnext;
+    ytr(end+1) = ynext;
+
+    
+    %% ==== Optional stuff for plotting and tracking error =========
+    
+    if jj==trialNumsToPlot(jplot) % Plot posterior over TC at beginning of this trial
+
+        subplot(2,npl,jplot); % ========= top plot ======
+        for j = 1:nslice
+            tc = ftc0(xx,thetasamps(j,:)); % tuning curve for this sample
+            plot(xx,tc,'color', colr,'linewidth', 2); hold on; % plot it
         end
-        h = plot(xx,ftc(xx),'k',xx,muTC,'r',xtr,ytr,'k.');
-        hold off;
-        set(h(3:end),'markersize',14); set(h,'linewidth',2);
-        set(gca,'ylim', [0 75]);
+        h = plot(xx,ftctrue(xx),'k',xx,TCmu,'r',xtr(1:end-1),ytr(1:end-1),'k.'); hold off;
+        set(h(3:end),'markersize',16); set(h,'linewidth',2);
+        set(gca,'ylim', [0 75],'xticklabel',[],'tickdir','out'); box off;
+        title(sprintf('trial %d', jj+ninit-1)); 
+        
         if jplot==1
             legend(h(1:2),{'true f', 'estimate'},'location','northwest');
             ylabel('spike rate (sp/s)');
         else
             set(gca,'yticklabel',[]);
         end
-        set(gca,'xticklabel',[]);set(gca,'tickdir','out');
-        title(sprintf('trial %d', jj+ntrain-1)); box off;
         
-        subplot(2,npl,npl+jplot); % =====================
+        subplot(2,npl,npl+jplot); % ====== bottom plot =============
         h=plot(xx,MI,xnext,MI(idx),'k*');
         set(h(1),'linewidth',2);
-        xlabel('x');box off;
+        xlabel('stimulus');box off;
+        set(gca,'ylim',[0 2],'tickdir','out');
 
-        % Compute errors
-        Errs(jj,1) = norm(tctrue-ftc0(xx,prsmu)); % error in estimate from posterior mean of parameters
-        Errs(jj,2) = norm(tctrue-muTC);  % error in estimate from posterior mean of TC
-        fprintf('(trial %d) Errs:  mean-parameters:%.2f  mean-TC:%.2f\n', jj, Errs(jj,:));
-        set(gca,'ylim',[0 2]);
-        set(gca,'tickdir','out');
         if jplot==1
-            ylabel('info gain (bits)');
-        else
-            set(gca,'yticklabel',[]);
+            ylabel('mutual information (bits)');
+            legend('MI', 'selected stimulus', 'location', 'northwest');
+        else 
+            set(gca,'yticklabel',[]); 
         end
-        jplot=jplot+1; % increment # of plots so far
+        
         drawnow;  
+        jplot=jplot+1; % increment # of plots so far
     end
         
-    % ======== end plotting code =======================
-    
-    % add new (stimulus,response) pair to the dataset
-    xtr(end+1) = xnext;
-    ytr(end+1) = ynext;
+    % Compute errors
+    Errs(jj,1) = norm(tctrue-ftc0(xx,thetamu)); % error in estimate from posterior mean of parameters
+    Errs(jj,2) = norm(tctrue-TCmu);  % error in estimate from posterior mean of TC
+    fprintf('Error (trial %2d):  mean-params=%7.2f  mean-TC=%7.2f\n', jj+ninit-1, Errs(jj,:));
+            
     
 end
